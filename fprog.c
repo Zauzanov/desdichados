149700cd2e57fa1a0c2945b9131f7db68db65720
Taulan Zauzanov <53187427+Zauzanov@users.noreply.github.com>
2026-02-10 00:26:19 +0300

    Update fprog.c

--- fprog.c
+++ fprog.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
 
+int main()
+{
+    int i;
+    for(i = 0; i < 10; i++)                 // 10 iterations
+    {
+        puts("SIGNAL COMPROMISED!\n");      // Prints
+    }
+    return 0;                               // Indicates the program ran successfully
+}
